458e7f4a71d1578abdf8cd145e75a596d179c6fa
lw <lw_coming@163.com>
2017-09-21 01:40:41 +0800

    删除信息

--- src/main/scala/cn/arvid/spark/ImportData.scala
+++ src/main/scala/cn/arvid/spark/ImportData.scala
@@ -10,9 +10,9 @@ object ImportData {
   def main(args: Array[String]): Unit = {
     var conf = new SparkConf().setAppName("testNeo4jWithSpark")
       .setMaster("local")
-      .set("spark.neo4j.bolt.url","bolt://10.2.0.139:7887")
+      .set("spark.neo4j.bolt.url","bolt://lw:7874")
       .set("spark.neo4j.bolt.user","neo4j")
-      .set("spark.neo4j.bolt.password","ksdc")
+      .set("spark.neo4j.bolt.password","123456")
     var sc:SparkContext = new SparkContext(conf)
 
     val neo = Neo4j(sc)
--- src/main/scala/cn/arvid/spark/ReadNeo4j.scala
+++ src/main/scala/cn/arvid/spark/ReadNeo4j.scala
@@ -1,7 +1,7 @@
 package cn.arvid.spark
 
 class ReadNeo4j {
-  private val context=new Context("NeoTest","bolt://10.2.0.139:7887","neo4j","ksdc")
+  private val context=new Context("NeoTest","bolt://lw:7874","neo4j","123456")
   val neo=context.neo;
   def query(labels: Array[String]){
     val rdd = neo.cypher("MATCH (n:Person) RETURN n.name").loadRowRdd
